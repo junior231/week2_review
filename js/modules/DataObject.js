304de850bb279899ba8a27770a961096f4db5587
Ilo <c_ilo@ats-hr420-dm33.fc.ca>
2019-09-09 19:59:04 -0400

    fixed it

---
+++ js/modules/DataObject.js
@@ -0,0 +1,14 @@
+// objects are collections of keys and values
+// remember to seperate the keys and values with a comma 
+// and thats about it
+// use a colon between the key and value
+
+
+const Person = {
+    name: "Collins",
+    eyeColor: "blue",
+    hairColour: "green",
+    role: "student"
+}
+
+export default Person;
